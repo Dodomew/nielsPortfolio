64dc8f209575370d04d4c8496e82a0117d6b35b0
Niels.Uiterwijk <nielsuiterwijk@gmail.com>
2018-04-29 13:15:10 +0200

    added images

--- index.php
+++ index.php
@@ -10,38 +10,66 @@
 	<body class="page">
 			<div class="mw1280 content">
 				<h1 class="content__site-title">Niels Uiterwijk</h1>
+				
 				<div class="image__container">
-					<div class="image__bg js-load_background_image" data-backgroundimage="http://via.placeholder.com/395x150">
+					<div class="image__bg js-load_background_image" data-backgroundimage="src/images/antegods.jpg">
 					</div>
 					<div class="image__meta">
-						<a href="#" class="image__title">Terraria</a>
+						<h1 class="image__title">Antegods</h1>
 						<p>lalalala</p>
 						<div class="image__icons"></div>
 					</div>
 				</div>
 				<div class="image__container">
-					<div class="image__bg js-load_background_image" data-backgroundimage="http://via.placeholder.com/395x150">
+					<div class="image__bg js-load_background_image" data-backgroundimage="src/images/terraria_mobile.jpg">
 					</div>
 					<div class="image__meta">
-						<a href="#" class="image__title">Terraria</a>
+						<h1 class="image__title">Terraria</h1>
 						<p>lalalala</p>
 						<div class="image__icons"></div>
 					</div>
 				</div>
 				<div class="image__container">
-					<div class="image__bg js-load_background_image" data-backgroundimage="http://via.placeholder.com/395x150">
+					<div class="image__bg js-load_background_image" data-backgroundimage="src/images/awesomenauts.jpg">
 					</div>
 					<div class="image__meta">
-						<a href="#" class="image__title">Terraria</a>
+						<h1 class="image__title">Awesomenauts</h1>
 						<p>lalalala</p>
 						<div class="image__icons"></div>
 					</div>
 				</div>
 				<div class="image__container">
-					<div class="image__bg js-load_background_image" data-backgroundimage="http://via.placeholder.com/395x150">
+					<div class="image__bg js-load_background_image" data-backgroundimage="src/images/ibbobb.jpg">
 					</div>
 					<div class="image__meta">
-						<a href="#" class="image__title">Terraria</a>
+						<h1 class="image__title">ibb & obb</h1>
+						<p>lalalala</p>
+						<div class="image__icons"></div>
+					</div>
+				</div>
+				<div class="image__container">
+					<div class="image__bg js-load_background_image" data-backgroundimage="src/images/rocket riot.jpg">
+					</div>
+					<div class="image__meta">
+						<h1 class="image__title">Rocket Riot 3D</h1>
+						<p>lalalala</p>
+						<div class="image__icons"></div>
+					</div>
+				</div>
+				<div class="image__container">
+					<div class="image__bg js-load_background_image" data-backgroundimage="src/images/swords soldiers ps3.jpg">
+					</div>
+					<div class="image__meta">
+						<h1 class="image__title">Swords & Soldiers</h1>
+						<p>lalalala</p>
+						<div class="image__icons"></div>
+					</div>
+				</div>
+				<div class="image__container">
+					<div class="image__bg js-load_background_image" data-backgroundimage="src/images/hiko.jpg">
+					</div>
+					<div class="image__meta">
+						<h1 class="image__title">Hiko</h1>
 						<p>lalalala</p>
 						<div class="image__icons"></div>
 					</div>
